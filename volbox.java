2c4f8cd9ce82a9c9f71694394d668d97e38d3281
Chirag Dewangan <93570903+chirag34661@users.noreply.github.com>
2022-09-13 11:03:35 +0530

    Add files via upload

---
+++ volbox.java
@@ -0,0 +1,10 @@
+public class Volume{
+    int x,y,z;
+public static void main(String a[]){
+      Volume A=new Volume();
+      A.x=10;
+      A.z=20;
+      A.y=22;
+  System.out.println("The volume of the box is "+ A.x*A.z*A.y);
+ }
+}						
