72f851cc44635fc2702316b9819ac6f87c7ffe7e
Chirag Dewangan <93570903+chirag34661@users.noreply.github.com>
2022-09-13 11:00:17 +0530

    Add files via upload

---
+++ artriangle.java
@@ -0,0 +1,9 @@
+public class Area{
+    int x,z;
+public static void main(String a[]){
+      Area A=new Area();
+      A.x=10;
+      A.z=20;
+  System.out.println("The area of the triangle is "+ (A.x*A.z)/2.0);
+ }
+}
