7cba67aa3c41a7458009b9588471bff96dfeaac3
Chirag Dewangan <93570903+chirag34661@users.noreply.github.com>
2022-09-20 10:24:51 +0530

    Delete Add.java

--- Add.java
+++
@@ -1,9 +0,0 @@
-class Add{
- public static void main(String a[ ]){
-  int x=10,y=20,z=30;
-  float f;
-  f=(float)x+y+z/3;  \\Type Casting
-  System.out.println("The sum of three numbers is " + (x+y+z));
-  System.out.println("Average is " + f );
- }
-}
