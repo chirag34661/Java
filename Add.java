b3d360654e56ab1518c40fc0ab59f66ecbe13e21
Chirag Dewangan <93570903+chirag34661@users.noreply.github.com>
2022-09-20 10:13:54 +0530

    Add files via upload
    #typecasting

---
+++ Add.java
@@ -0,0 +1,9 @@
+class Add{
+ public static void main(String a[ ]){
+  int x=10,y=20,z=30;
+  float f;
+  f=(float)x+y+z/3;
+  System.out.println("The sum of three numbers is " + (x+y+z));
+  System.out.println("Average is " + f );
+ }
+}
