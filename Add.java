706bdb7fa2e270c934aa4658270abf131911774c
Chirag Dewangan <93570903+chirag34661@users.noreply.github.com>
2022-09-20 10:14:28 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -2,7 +2,7 @@ class Add{
  public static void main(String a[ ]){
   int x=10,y=20,z=30;
   float f;
-  f=(float)x+y+z/3;
+  f=(float)x+y+z/3;  \\Type Casting
   System.out.println("The sum of three numbers is " + (x+y+z));
   System.out.println("Average is " + f );
  }
